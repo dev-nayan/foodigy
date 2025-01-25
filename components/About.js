3c36d976c5a0d135f874050fc1d8de31852727c3
nayan <jhanayan543@gmail.com>
2025-01-25 16:16:47 +0530

    changes in about page

--- components/About.js
+++ components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="About-cont">
-      <h1> Hello Iam About</h1>
+      <h1> Hello I am About Page</h1>
     </div>
   );
 };
